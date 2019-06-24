166dd8d806c07befa7d68163b0aa6bd9d62e9a72
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2019-06-24 18:59:34 +0100

    Build basic test program on VS2010

--- testAsynPortClientApp/src/testSerialPort.cpp
+++ testAsynPortClientApp/src/testSerialPort.cpp
@@ -8,9 +8,13 @@
 #include <drvAsynSerialPort.h>
 #include <asynPortClient.h>
 
-#include <CLI/CLI.hpp>
+#if defined(_MSC_VER) && _MSC_VER < 1900
+#define NO_CLI11_SUPPORT
+#endif
 
-// testSerialPort [options] COMPORT [outputString] [outputEos] [inputEos]
+#ifndef NO_CLI11_SUPPORT
+
+#include <CLI/CLI.hpp>
 
 static CLI::Validator CheckYesNo = CLI::IsMember({"Y", "N"}, CLI::ignore_case);
 static CLI::Validator CheckNumber = CLI::Number;
@@ -20,7 +24,9 @@ static void addOption(CLI::App& app, std::map<std::string, std::string>& optMap,
 {
     app.add_option(std::string("--") + opt, optMap[opt], desc)->check(validator);
 }	
-	
+
+// testSerialPort [options] COMPORT [outputString] [outputEos] [inputEos]
+
 int main(int argc, char *argv[])
 {
   CLI::App app;
@@ -118,3 +124,96 @@ int main(int argc, char *argv[])
   }
   return 0;
 }
+
+#else
+
+// VS2010 version with no options support
+
+// testSerialPort COMPORT [outputString] [outputEos] [inputEos]
+
+int main(int argc, char *argv[])
+{
+  std::string serial_port;
+  std::string output_string;
+  std::string output_eos;
+  std::string input_eos;
+  int serial_report = 5;
+  bool noread = false;
+  
+  if (argc > 1)
+  {
+      serial_port = argv[1];
+  }
+  if (argc > 2)
+  {
+      output_string = argv[2];
+  }
+  if (argc > 3)
+  {
+      output_eos = argv[3];
+  }
+  if (argc > 4)
+  {
+      input_eos = argv[4];
+  }
+  
+  char input[10000], inputEos[10], outputEos[10];
+  size_t nIn = 0, nOut = 0;
+  int eomReason = 0;
+  asynStatus readStatus, writeStatus;
+
+  drvAsynSerialPortConfigure("L0", serial_port.c_str(), 0, 0, 0);
+  
+  std::auto_ptr<asynOctetClient> client(new asynOctetClient("L0", 0, 0));
+  std::auto_ptr<asynOptionClient> optClient(new asynOptionClient("L0", 0, 0));
+
+  optClient->setOption("eventmask", "0x1ff");
+  
+  if (output_eos.size() > 0) {
+      epicsStrnRawFromEscaped(outputEos, sizeof(outputEos), output_eos.c_str(), output_eos.size());
+      client->setOutputEos(outputEos, static_cast<int>(strlen(outputEos)));
+  }
+  if (input_eos.size() > 0) {
+      epicsStrnRawFromEscaped(inputEos, sizeof(inputEos), input_eos.c_str(), input_eos.size());
+      client->setInputEos(inputEos, static_cast<int>(strlen(inputEos)));
+  }
+
+  if (output_string.size() > 0)
+  {
+      if (serial_report >= 0)
+      {
+	      printf("\n--- Report prior to write ---\n\n");
+		  pasynManager->report(stdout, serial_report, "L0");
+      } 
+      writeStatus = client->write(output_string.c_str(), output_string.size(), &nOut);
+  }
+  
+  if (!noread)
+  {
+      if (serial_report >= 0)
+      {
+	      printf("\n--- Report prior to read ---\n\n");
+		  pasynManager->report(stdout, serial_report, "L0");
+      }
+	  input[0] = '\0';
+      readStatus = client->read(input, sizeof(input), &nIn, &eomReason);
+  }
+
+  if (serial_report >= 0)
+  {
+ 	  printf("\n--- Report at end ---\n\n");
+      pasynManager->report(stdout, serial_report, "L0");
+  }
+  
+  if (output_string.size() > 0)
+  {
+      printf("Sent %d bytes, status=%s, output:\n%s\n", (int)nOut, pasynManager->strStatus(writeStatus), output_string.c_str());
+  }
+  if (!noread)
+  {
+      printf("Received %d bytes, status=%s, eomReason=%d, response:\n%s\n", (int)nIn, pasynManager->strStatus(readStatus), eomReason, input);
+  }
+  return 0;
+}
+
+#endif /* ifndef NO_CLI11_SUPPORT */
